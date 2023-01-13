3921baa22a4fbb9578fafe621380b98b8df1f127
hamidreza <hamidrezakhoramrokh@gmail.com>
2023-01-13 12:57:36 -0500

    tiptap adoption

---
+++ svelte/svelte-frontend/src/lib/tiptap.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import { onMount, onDestroy } from 'svelte'
+    import { Editor } from '@tiptap/core'
+    import StarterKit from '@tiptap/starter-kit'
+  
+    let element: any
+    let editor: any
+  
+    onMount(() => {
+      editor = new Editor({
+        element: element,
+        extensions: [
+          StarterKit,
+
+        ],
+        content: '<p>this is bannana 🌍️ </p>',
+        onTransaction: () => {
+          // force re-render so `editor.isActive` works as expected
+          editor = editor
+        },
+      })
+    })
+  
+    onDestroy(() => {
+      if (editor) {
+        editor.destroy()
+      }
+    })
+    const save = () => {
+        const json = editor.getJSON()
+        console.log(json)
+    }
+    const wholeDelete = () => {
+        editor.commands.clearContent()
+    }
+  </script>
+  
+  {#if editor}
+    <button
+      on:click={() => editor.chain().focus().toggleHeading({ level: 1}).run()}
+      class:active={editor.isActive('heading', { level: 1 })}
+    >
+      H1
+    </button>
+    <button
+      on:click={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+      class:active={editor.isActive('heading', { level: 2 })}
+    >
+      H2
+    </button>
+    <button on:click={() => editor.chain().focus().setParagraph().run()} class:active={editor.isActive('paragraph')}>
+      P
+    </button>
+    <button on:click={wholeDelete}>del</button>
+  {/if}
+  
+  <div bind:this={element} />
+  
+  <style>
+    button.active {
+      background: black;
+      color: white;
+    }
+  </style>
+  
---
+++ svelte/svelte-frontend/src/routes/notes/+page.svelte
@@ -0,0 +1,56 @@
+<!-- <script>
+	import {  onMount } from 'svelte'
+	import Quill from "quill";
+	let quill = null;
+	 
+	
+  onMount( () => { 
+		
+		let container = document.getElementById('editor');
+		quill = new Quill(container, {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, 3, false] }],
+        ["bold", "italic", "underline", "strike"],
+        ["link", "code-block"]
+      ]
+    },
+    placeholder: "Type something...",
+    theme: "snow" // or 'bubble'
+  });
+	})
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  #editor {
+    height: 350px;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
+<svelte:head>
+	<link href="//cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+</svelte:head>
+<main>
+  <h1>Hey</h1>
+  <div id="editor" />
+</main> -->
+
+<script>
+    import Tiptap from '../../lib/tiptap.svelte';
+  </script>
+  
+  <main>
+    <Tiptap />
+  </main>
