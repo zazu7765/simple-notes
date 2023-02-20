163306af92d212d9bd9f0d888f09df9355e8e31e
HamidReza <hr.kh1380@gmail.com>
2023-02-19 19:01:32 -0500

    notebooks

--- svelte/svelte-frontend/src/routes/user/[slug]/+page.svelte
+++ svelte/svelte-frontend/src/routes/user/[slug]/+page.svelte
@@ -6,8 +6,12 @@
 	export let data: any;
 	let login = data.bool;
 	$write = data['responseNote']
-	
-	console.log(data['responseNote'])
+	let pp;
+	write.subscribe(value => {
+		pp = value;
+	});
+	console.log(pp["Data"]+' note');
+	// console.log(data['responseNote'])
 	
 	let num = 5;
 	console.log(data);
@@ -46,7 +50,7 @@
           </li>
 		  <ul class="pt-4 mt-4 space-y-2 border-t border-gray-500 dark:border-gray-700"></ul>
 		  <li>
-		  <button type="button" class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+		  <!-- <button type="button" class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
 			<svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd"></path></svg>
 			<span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>E-commerce</span>
 			<svg sidebar-toggle-item class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -62,7 +66,7 @@
 			   <a href="#" class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
 			</li>
 	  </ul>
-   </li>
+   </li> -->
 		  {#each arr as name, index}
 		  <Sidebar {name} />
 	  {/each}
