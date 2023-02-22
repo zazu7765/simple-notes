82b36cae15900e59df0e40b2a8d6c25447a0d7ac
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2023-02-22 17:32:48 -0500

    bug fixed

--- svelte/svelte-frontend/src/routes/user/[slug]/+page.svelte
+++ svelte/svelte-frontend/src/routes/user/[slug]/+page.svelte
@@ -29,7 +29,8 @@
 		ww = "w-64"
 	}
 	function closeNav() {
-	  ww = "w-0";
+	  ww = "w-0 hidden";
+
 	}
 </script>
 
@@ -39,14 +40,14 @@
 	openNav();
 }} type="button" class="Class
 
-z-50 inline-flex  items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+z-50 inline-flex  items-center p-6 mt-2 ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
 	<span class="sr-only">Open sidebar</span>
 	<svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 	   <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
 	</svg>
  </button>
  
- <aside class="ease-in-out duration-300 fixed top-0 left-0 z-40 {ww} h-screen sm:translate-x-0" aria-label="Sidebar">
+ <aside class="ease-in-out duration-300 fixed top-0 left-0 z-40 {ww} h-screen " aria-label="Sidebar">
 	<div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
 	   <ul class="space-y-2">
 		
