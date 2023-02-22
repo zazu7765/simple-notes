b6c6eebd6ba60ea9bb6388b0d98c6fe5d31bfa96
daniel <ohmyzazu@gmail.com>
2023-02-22 18:48:01 -0500

    onclick frost

--- svelte/svelte-frontend/src/routes/user/[slug]/+page.svelte
+++ svelte/svelte-frontend/src/routes/user/[slug]/+page.svelte
@@ -123,7 +123,7 @@ z-50 inline-flex  items-center p-6 mt-2 ml-3 text-sm text-gray-500 rounded-lg ho
 		</div>
 	</aside>
 
-	<div id="main" class="p-4 {blur} inset-0">
+	<div id="main" class="p-4 {blur} inset-0" on:click={()=>{closeNav()}}>
 		<div
 			class="p-4 border-2 bg-white border-gray-500 border-dashed rounded-lg dark:border-gray-700"
 		>
