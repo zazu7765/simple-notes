78b3e528d8ff86dbd316aa15734832e6496682ea
Hamid Reza Khoramrokh <hr.kh1380@gmail.com>
2023-03-01 12:31:09 -0500

    page

--- svelte/svelte-frontend/src/routes/sign/+page.svelte
+++ svelte/svelte-frontend/src/routes/sign/+page.svelte
@@ -9,18 +9,14 @@
 
 <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
 	<div class="w-full max-w-md space-y-8 p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-		<div>
-			<img
-				class="mx-auto h-12 w-auto"
-				src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-				alt="Your Company"
-			/>
-			<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+		<div class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+			Simple notes🗒️
+			<h2 class="mt-6 text-center text-2xl font-bold tracking-tight text-gray-900">
 				Sign up for a new account!
 			</h2>
 			<!-- <p class="mt-2 text-center text-sm text-gray-600">
           Or
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+          <a href="#" class="font-medium text-gray-600 hover:text-gray-500">start your 14-day free trial</a>
         </p> -->
 		</div>
 		<form class="mt-8 space-y-6" method="POST">
@@ -32,7 +28,7 @@
 						id="name"
 						name="name"
 						required
-						class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+						class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
 						placeholder="Name"
 					/>
 				</div>
@@ -44,7 +40,7 @@
 						type="email"
 						autocomplete="email"
 						required
-						class="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+						class="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
 						placeholder="Email address"
 					/>
 				</div>
@@ -56,7 +52,7 @@
 						type="password"
 						autocomplete="current-password"
 						required
-						class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+						class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-gray-500 focus:outline-none focus:ring-gray-500 sm:text-sm"
 						placeholder="Password"
 					/>
 				</div>
@@ -68,14 +64,14 @@
 						id="remember-me"
 						name="remember-me"
 						type="checkbox"
-						class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+						class="h-4 w-4 rounded border-gray-300 text-gray-600 focus:ring-gray-500"
 					/> -->
 					<!-- <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label> -->
 				<!-- </div> -->
 				<!-- probebly dont need password recovery -->
 
 				<!-- <div class="text-sm">
-					<a href="../notes" class="font-medium text-indigo-600 hover:text-indigo-500"
+					<a href="../notes" class="font-medium text-gray-600 hover:text-gray-500"
 						>Forgot your password?</a
 					>
 				</div> -->
@@ -84,12 +80,12 @@
 			<div>
 				<button
 					type="submit"
-					class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+					class="group relative flex w-full justify-center rounded-md border border-transparent bg-gray-500 py-2 px-4 text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
 				>
 					<span class="absolute inset-y-0 left-0 flex items-center pl-3">
 						<!-- Heroicon name: mini/lock-closed -->
 						<svg
-							class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+							class="h-5 w-5 group-hover:text-slate-500"
 							xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 0 20 20"
 							fill="currentColor"
